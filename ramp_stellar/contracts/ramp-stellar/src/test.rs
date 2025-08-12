f45fd38fd6660538c15e8e4c39da431e3c1228a9
Phidel Musungu <p.musungu@supraoracles.com>
2025-08-12 15:57:15 +0300

    stellar token implementations

--- ramp_stellar/contracts/ramp-stellar/src/test.rs
+++ ramp_stellar/contracts/ramp-stellar/src/test.rs
@@ -7,8 +7,8 @@ use soroban_sdk::{testutils::Address as _, Env, Address};
 fn test() {
     let env = Env::default();
     let test_admin = Address::generate(&env);
-    let contract_id = env.register(RampContract, (test_admin, 10u32));
+    let contract_id = env.register(RampContract, (test_admin.clone(), 10u32));
     let client = RampContractClient::new(&env, &contract_id);
-
-    client.add_asset( &Address::generate(&env), &10u128);
+    
+    client.add_asset( &Address::generate(&env), &test_admin, &10i128);
 }
